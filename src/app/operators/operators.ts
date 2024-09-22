6560b3233b32fe8a3e874395a382e2cb71110d13
Shlomi Shitrit <akunh113@gmail.com>
2024-09-22 12:08:11 +0300

    init project

---
+++ src/app/operators/operators.ts
@@ -0,0 +1,21 @@
+import {
+    greaterOperator,
+    lowerOperator,
+    equalOperator,
+    rangeOperator,
+    startWithOperator,
+    endWithOperator,
+    containsOperator,
+    equalStringOperator,
+} from "@/app/operators";
+
+export const OPERATORS = {
+    greater: greaterOperator,
+    lower: lowerOperator,
+    equal: equalOperator,
+    range: rangeOperator,
+    startWith: startWithOperator,
+    endWith: endWithOperator,
+    contains: containsOperator,
+    equalString: equalStringOperator,
+};
